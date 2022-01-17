3bf6ac5817911923d743cf468c330b156e3023bc
Anton van Pelt <anton@antonvanpelt.com>
2022-01-17 22:43:06 +0100

    Az files 1st commit

---
+++ terraform/azure/azfiles.tf
@@ -0,0 +1,40 @@
+resource "random_integer" "azurefiles" {
+  min = 1000000
+  max = 9999999
+}
+
+resource "azurerm_storage_account" "azurefiles" {
+  name                     = "sainfra${local.environment_abbreviations[terraform.workspace]}${random_integer.azurefiles.result}"
+  resource_group_name      = azurerm_resource_group.azurefiles.name
+  location                 = azurerm_resource_group.azurefiles.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "azurefiles" {
+  name                 = "share"
+  storage_account_name = azurerm_storage_account.azurefiles.name
+  quota                = 50
+
+#  acl {
+#    id = "MTIzNDU2Nzg5MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTI"
+#
+#    access_policy {
+#      permissions = "rwdl"
+#      start       = "2022-01-01T09:38:21.0000000Z"
+#      expiry      = "2022-07-02T10:38:21.0000000Z"
+#    }
+#  }
+}
+
+resource "azurerm_storage_share_directory" "software" {
+  name                 = "software"
+  share_name           = azurerm_storage_share.azurefiles.name
+  storage_account_name = azurerm_storage_account.azurefiles.name
+}
+
+resource "azurerm_storage_share_file" "dummyfile" {
+  path             = azurerm_storage_share_directory.software.name
+  name             = "azurefiles.txt"
+  storage_share_id = azurerm_storage_share.azurefiles.id
+}
